8c6a20900f9ae584f58069e29531a460d5d76d8c
padils <bobo-00@inbox.ru>
2020-07-11 18:58:35 +0300

    Updates

---
+++ precache-manifest.070bd5f2976ad09a1e46d602e6a5aeb1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f27ffe4b142a1c0fbed3ebe2c70daac",
+    "url": "/cart/index.html"
+  },
+  {
+    "revision": "616b38f57bad3f17a1b6",
+    "url": "/cart/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "3b4ea79db0e8153fb8c9",
+    "url": "/cart/static/css/main.9dba5bbd.chunk.css"
+  },
+  {
+    "revision": "616b38f57bad3f17a1b6",
+    "url": "/cart/static/js/2.d7a4273f.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/cart/static/js/2.d7a4273f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3b4ea79db0e8153fb8c9",
+    "url": "/cart/static/js/main.da30b65c.chunk.js"
+  },
+  {
+    "revision": "8eb900fa4e76619071c5",
+    "url": "/cart/static/js/runtime-main.2354bd9f.js"
+  },
+  {
+    "revision": "c8f026d24bafd5ab91619dabf7183f4c",
+    "url": "/cart/static/media/bottle.c8f026d2.png"
+  }
+]);
